670ea128d149d64855a3f28e8d6c05abe102c30f
Yaroslav3 <iaroslav_111@ukr.net>
2020-11-19 02:32:26 +0200

    table component

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -7,15 +7,21 @@ import {Models} from '../../core/interfaces/interfaces';
   styleUrls: ['./table.component.sass']
 })
 export class TableComponent implements OnInit, OnChanges{
+  public isEmptyList = false;
   @Input() gitHub: Models.GitHub[];
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.isEmptyList = true;
   }
 
   ngOnChanges(): void {
-    console.log('gitHub', this.gitHub);
+    this.isEmptyList = this._isCheckListIsEmpty(this.gitHub);
+  }
+
+  private _isCheckListIsEmpty(gitArray: Models.GitHub[]): boolean {
+    return !gitArray.length;
   }
 }
